d47d9e38bebe91bf206e614ad44d92721dbfb163
Valentine Makarov <octopulus96@gmail.com>
2021-09-26 20:17:55 +0300

    Добавил ручку добавляющую водопад в список

--- service/__main__.py
+++ service/__main__.py
@@ -1,12 +1,21 @@
-from service.clients.twitter import Twitter
-
+waterfalls = [{
+    "title": "Ниагарский водопад",
+    "description": "Самый большой водопад в мире, располагающийся на границе США и Канады",
+    "height": 120, # высота в метрах
+    "size": 2000 # ширина в метрах
+}]
 
 def main():
-    print('hi')
 
-    twitter_client = Twitter(token='sdkfsfldskjflkdsjfs')
-    twitter_client.connect()
-
+    def new_waterfall():
+        new_dict = dict.fromkeys(['title','description', 'height','size'])
+        new_dict['title'] = input('Имя водопада\n')
+        new_dict['description'] = input('Его описание\n')
+        new_dict['height'] = input('Его высота в метрах\n')
+        new_dict['size'] = input('Его ширина в метрах\n')
+        waterfalls.append(new_dict)
+    
+    new_waterfall()
 
 if __name__ == '__main__':
     main()
