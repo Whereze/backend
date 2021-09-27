869985b6393418a7b607ee6c2b33b4af880944b8
Valentine Makarov <octopulus96@gmail.com>
2021-09-27 17:09:50 +0300

    Сделал набросок 2 ручек

--- service/__main__.py
+++ service/__main__.py
@@ -1,12 +1,31 @@
-from service.clients.twitter import Twitter
-
+waterfalls = [{
+    "uid": 1,
+    "title": "Ниагарский водопад",
+    "description": "Самый большой водопад в мире, располагающийся на границе США и Канады",
+    "height": 120, # высота в метрах
+    "size": 2000 # ширина в метрах
+}]
 
 def main():
-    print('hi')
 
-    twitter_client = Twitter(token='sdkfsfldskjflkdsjfs')
-    twitter_client.connect()
+    def new_waterfall():
+        new_dict = dict.fromkeys(['uid', 'title', 'description', 'height', 'size'])
+        new_dict['uid'] = 2
+        new_dict['title'] = input('Имя водопада\n')
+        new_dict['description'] = input('Его описание\n')
+        new_dict['height'] = input('Его высота в метрах\n')
+        new_dict['size'] = input('Его ширина в метрах\n')
+        waterfalls.append(new_dict)
+    
+    def take_uid_waterfall():
+        uid_waterfall = int(input())
+        for waterfall in waterfalls:
+            uid = waterfall['uid']
+            if uid == uid_waterfall:
+                print(waterfall)
 
+    new_waterfall()
+    take_uid_waterfall()
 
 if __name__ == '__main__':
     main()
