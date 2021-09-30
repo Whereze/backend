7c397918cf894f254a213e47fd8a5632a662dc84
Valentine Makarov <octopulus96@gmail.com>
2021-09-30 13:34:56 +0300

    doing migration sqlalchemy

--- service/models/db.py
+++ service/models/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-engine = create_engine()
-db_session = declarative_base()
+engine = create_engine('postgresql://whereze:whereze@localhost:5432/whereze')
+db_session = scoped_session(sessionmaker(bing=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
