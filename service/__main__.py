fddd67ba3b9669b36824a558c1be47283fb0eb96
Valentine Makarov <octopulus96@gmail.com>
2021-09-30 11:54:57 +0300

    Added handles for list waterfall

--- service/__main__.py
+++ service/__main__.py
@@ -1,21 +1,13 @@
-waterfalls = [{
-    "title": "Ниагарский водопад",
-    "description": "Самый большой водопад в мире, располагающийся на границе США и Канады",
-    "height": 120, # высота в метрах
-    "size": 2000 # ширина в метрах
-}]
+from flask import Flask
+from service.functional.handles import *
 
-def main():
-
-    def new_waterfall():
-        new_dict = dict.fromkeys(['title','description', 'height','size'])
-        new_dict['title'] = input('Имя водопада\n')
-        new_dict['description'] = input('Его описание\n')
-        new_dict['height'] = input('Его высота в метрах\n')
-        new_dict['size'] = input('Его ширина в метрах\n')
-        waterfalls.append(new_dict)
-    
-    new_waterfall()
+app = Flask(__name__)
+@app.route("/")
+def uid_waterlall():
+    new_waterfall('Рейнский водопад', 'Рейнский водопад считается самым большим равнинным водопадом в Европе',
+    150, 23)
+    waterfall = take_uid_waterfall(2)
+    return waterfall
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
---
+++ service/functional/handles.py
@@ -0,0 +1,22 @@
+waterfalls = [{
+    "uid": 1,
+    "title": "Ниагарский водопад",
+    "description": "Самый большой водопад в мире, располагающийся на границе США и Канады",
+    "height": 120, # высота в метрах
+    "size": 2000 # ширина в метрах
+}]
+
+def take_uid_waterfall(uid):   
+    for waterfall in waterfalls:
+        uid_waterlall = waterfall['uid']
+        if uid == uid_waterlall:
+            return f'''{waterfall['title']} - {waterfall['description']}'''
+
+def new_waterfall(title, description, height, size):
+    new_dict = dict.fromkeys(['uid', 'title', 'description', 'height', 'size'])
+    new_dict['uid'] = 2
+    new_dict['title'] = title
+    new_dict['description'] = description
+    new_dict['height'] = height
+    new_dict['size'] = size
+    waterfalls.append(new_dict)
