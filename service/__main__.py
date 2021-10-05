eab207661cd67098130f444e721d85b3b0dab111
Valentine Makarov <octopulus96@gmail.com>
2021-10-05 17:40:15 +0300

    chore: service edited in style flake8

--- service/__main__.py
+++ service/__main__.py
@@ -1,32 +1,36 @@
 from flask import Flask, jsonify, request
 
-from service.functional.handles import new_waterfall, waterfalls #Загружаю список водопадов
+from service.functional.handles import waterfalls
+#  Загружаю список водопадов
 
 app = Flask(__name__)
 
+
 client = app.test_client()
 
-@app.route("/api/v1/waterfalls/", methods = ['GET'])
+
+@app.route("/api/v1/waterfalls/", methods=['GET'])
 def get_waterfalls():
     return jsonify(waterfalls)
 
-@app.route("/api/v1/waterfalls/", methods = ['POST'])
+
+@app.route("/api/v1/waterfalls/", methods=['POST'])
 def post_waterfalls():
     new_waterfall = request.json
     waterfalls.append(new_waterfall)
     return jsonify(new_waterfall)
 
-@app.route("/api/v1/waterfalls/<int:uid>/", methods = ['GET'])
+
+@app.route("/api/v1/waterfalls/<int:uid>/", methods=['GET'])
 def get_uid_waterfalls(uid):
     return jsonify(waterfalls[uid])
 
-@app.route("/api/v1/waterfalls/<int:uid>/", methods = ['PUT'])
+
+@app.route("/api/v1/waterfalls/<int:uid>/", methods=['PUT'])
 def put_waterfalls(uid):
     changes = request.json
     waterfalls[uid].update(changes)
     return jsonify(waterfalls[uid])
 
-#TEST POST
-#curl -X POST -H 'Content-Type: application/json' \ -d '{"title":"Рейнский водопад", "description": "Рейнский водопад считается самым большим равнинным водопадом в Европе"}' http://localhost:5000/
 if __name__ == '__main__':
     app.run(debug=True)
