d3b6b841053a1e041b1de6748e2680ccaab3d9f0
Valentine Makarov <octopulus96@gmail.com>
2021-10-05 17:55:11 +0300

    chore: handles edited in style flake8

--- service/functional/handles.py
+++ service/functional/handles.py
@@ -1,16 +1,19 @@
 waterfalls = [{
     "uid": 1,
     "title": "Ниагарский водопад",
-    "description": "Самый большой водопад в мире, располагающийся на границе США и Канады",
-    "height": 120, # высота в метрах
-    "size": 2000 # ширина в метрах
+    "description": """Самый большой водопад в мире,
+    располагающийся на границе США и Канады""",
+    "height": 120,  # высота в метрах
+    "size": 2000  # ширина в метрах
 }]
 
-def take_uid_waterfall(uid):   
+
+def take_uid_waterfall(uid):
     for waterfall in waterfalls:
         uid_waterlall = waterfall['uid']
         if uid == uid_waterlall:
             return f'''{waterfall['title']} - {waterfall['description']}'''
+
 
 def new_waterfall(title, description, height, size):
     new_dict = dict.fromkeys(['uid', 'title', 'description', 'height', 'size'])
@@ -21,4 +24,4 @@ def new_waterfall(title, description, height, size):
     new_dict['size'] = size
     waterfalls.append(new_dict)
 
-#Уже не актуально, но пусть будет
+#  Уже не актуально, но пусть будет
