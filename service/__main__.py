893109e31837844fd8424db56aa5c4e316cd7738
Anna <anna.dvoskina@yandex.ru>
2021-10-05 19:30:30 +0300

    added exceptions handling for handlers

--- service/__main__.py
+++ service/__main__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,render_template
 import re
 
 from service.functional.handles import new_waterfall, waterfalls #Загружаю список водопадов
@@ -7,30 +7,64 @@ app = Flask(__name__)
 
 client = app.test_client()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'errorCode' : 404, 'message' : "Woops, that page doesn't exist!"})
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return jsonify({'errorCode' : 500, 'message' : "Internal server mistake :("})
+
 @app.route("/api/v1/waterfalls/", methods = ['GET'])
 def get_waterfalls():
-    return jsonify(waterfalls)
+    try:
+        if waterfalls:
+            return jsonify(waterfalls)
+        else:
+            return 'No waterfalls yet'
+    except(ValueError,TypeError):
+        return 'Change your request'
 
 @app.route("/api/v1/waterfalls/", methods = ['POST'])
 def post_waterfalls():
-    new_waterfall = request.json
-    waterfalls.append(new_waterfall)
-    return jsonify(new_waterfall)
+    try:
+        new_waterfall = request.json
+        waterfalls.append(new_waterfall)
+        return jsonify(new_waterfall)
+    except(ValueError,TypeError):
+        return 'Change your request'
 
 @app.route("/api/v1/waterfalls/<int:uid>/", methods = ['GET'])
 def get_uid_waterfalls(uid):
-    return jsonify(waterfalls[uid])
+    try:
+        waterfall = waterfalls[uid]
+        if waterfall:
+            return jsonify(waterfalls[uid])
+        else:
+            return 'Waterfall not found'
+    except(KeyError,ValueError,TypeError,IndexError):
+        return 'Change your request'
 
 @app.route("/api/v1/waterfalls/<int:uid>/", methods = ['PUT'])
 def put_waterfalls(uid):
-    changes = request.json
-    waterfalls[uid].update(changes)
-    return jsonify(waterfalls[uid])
+    try:
+        changes = request.json
+        waterfalls[uid].update(changes)
+        return jsonify(waterfalls[uid])
+    except(KeyError,ValueError,TypeError,IndexError):
+        return 'Change your request'
+
 
 @app.route("/api/v1/waterfalls/<string:key>/<string:text>/", methods = ['GET'])
 def get_name_waterfalls(key,text):
-    waterfall = list(filter(lambda waterfall: re.findall(text,waterfall[key], re.IGNORECASE), waterfalls))
-    return jsonify(waterfall)
+    try:
+        waterfall = list(filter(lambda waterfall: re.findall(text,waterfall[key], re.IGNORECASE), waterfalls))
+        if waterfall:
+            return jsonify(waterfall)
+        else:
+            return 'Waterfall not found'
+    except(KeyError,ValueError,TypeError):
+        return 'Change key or value and try again'
 
 #TEST POST
 #curl -X POST -H 'Content-Type: application/json' \ -d '{"title":"Рейнский водопад", "description": "Рейнский водопад считается самым большим равнинным водопадом в Европе"}' http://localhost:5000/
