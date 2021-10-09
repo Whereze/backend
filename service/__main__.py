0b4413a589a0574ce81cb46bc12a771ba7a71245
Anna <anna.dvoskina@yandex.ru>
2021-10-09 17:05:48 +0300

    validated put request

--- service/__main__.py
+++ service/__main__.py
@@ -1,22 +1,12 @@
 from flask import Flask, jsonify, request, abort
 from pydantic import ValidationError
-from typing import Optional
 import re
-from pydantic.main import BaseModel
 from service.functional.handles import waterfalls
-from service.functional.waterfalls_basemodel import Waterfall_model
+from service.serializers import Waterfall
 
 app = Flask(__name__)
 
 client = app.test_client()
-
-
-class Waterfall(BaseModel):
-    uid: Optional[int]
-    title: Optional[str]
-    description: Optional[str]
-    height: Optional[int]
-    size: Optional[int]
 
 
 @app.errorhandler(404)
@@ -64,15 +54,12 @@ def get_uid_waterfalls(uid):
 @app.route("/api/v1/waterfalls/<int:uid>/", methods=['PUT'])
 def put_waterfalls(uid):
     try:
-        try:
-            changes = request.json
-            changes = Waterfall_model(**changes)
-            waterfalls[uid].update(changes)
-            return jsonify(waterfalls[uid])
-        except ValidationError as e:
-            return e.json()
-    except(KeyError, ValueError, TypeError, IndexError):
-        return 'Change your request'
+        changes = request.json
+        changes = Waterfall(**changes)
+        waterfalls[uid].update(changes)
+        return jsonify(waterfalls[uid])
+    except ValidationError as e:
+        abort(400, str(e))
 
 
 @app.route("/api/v1/waterfalls/<string:key>/<string:text>/", methods=['GET'])
@@ -89,4 +76,4 @@ def get_name_waterfalls(key, text):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
