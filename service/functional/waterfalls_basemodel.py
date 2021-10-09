29e4e33176dd966129ab46156ba69eb6d6dd9518
Anna <anna.dvoskina@yandex.ru>
2021-10-09 17:05:07 +0300

    renamed waterfall name

--- service/functional/waterfalls_basemodel.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic.main import BaseModel
-
-class Waterfall_model(BaseModel):
-    uid: Optional[int]
-    title: Optional[str]
-    description: Optional[str]
-    height: Optional[int]
-    size: Optional[int]
