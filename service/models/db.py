f129bb2ddbe0510d79d08bec31edbb6a68b6b808
Valentine Makarov <octopulus96@gmail.com>
2021-10-11 17:35:20 +0300

    chore: corrected a typo

--- service/models/db.py
+++ service/models/db.py
@@ -6,7 +6,7 @@ import os
 
 db_url = os.environ['DB_URL']
 engine = create_engine(db_url)
-db_session = scoped_session(sessionmaker(bing=engine))
+db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
