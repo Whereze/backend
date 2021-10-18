2d7d267fcd555a6707f04bf661c7dc39a7389f81
Anna <anna.dvoskina@yandex.ru>
2021-10-18 11:21:13 +0300

    made all fields optional as could not get nullable fields

--- service/serializers.py
+++ service/serializers.py
@@ -4,14 +4,13 @@ from typing import Optional
 
 class WaterfallModel(BaseModel):
     uid: Optional[int]
-    title: str
-    summary: str
-    height: str
-    width: str
-    river: str
-    country: str
-    region: str
-    RF_subject: str
+    title: Optional[str]
+    summary: Optional[str]
+    width: Optional[str]
+    river: Optional[str]
+    country: Optional[str]
+    region: Optional[str]
+    RF_subject: Optional[str]
 
     def __repr__(self) -> str:
         return f"WaterfallModel({self.uid}{self.title})"
